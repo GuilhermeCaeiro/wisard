0cb64802ecbc3fe74415b9df50c60d2319e393f6
Guilherme Caeiro de Mattos <GuilhermeCaeiro@users.noreply.github.com>
2020-04-12 12:29:24 -0300

    Fixed some errors in the tests and added tests for mental image generation.
    The error that was fixed is that when Wisard objects were created, the parameters passed for bleaching (boolean) and seed (integet) were swapped. The tests also reflect recent changes in the Wisard class.

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 from Wisard import Wisard
+import matplotlib.pyplot as plt
+import numpy as np
 
 # Just a very simple test to see if things are working.
 
@@ -24,14 +26,16 @@ test_set = [
 training_patterns = [row[0] for row in training_set]
 expected_outputs = [row[1] for row in training_set]
 
-wann = Wisard(2, 3546, False)
+wann = Wisard(2, False, 3546)
 wann.train(training_patterns, expected_outputs)
 
-for discriminator in wann.discriminators:
-    print(discriminator.input_class)
-    print(discriminator.input_length)
-    print(discriminator.tuple_size)
-    print(discriminator.memory)
+print(wann.get_mental_images())
+
+for discriminator_class in wann.discriminators:
+    print(wann.discriminators[discriminator_class].input_class)
+    print(wann.discriminators[discriminator_class].input_length)
+    print(wann.discriminators[discriminator_class].tuple_size)
+    print(wann.discriminators[discriminator_class].memory)
 
 
 print("\n\n\n")
@@ -40,54 +44,17 @@ for pattern in test_set:
     print("Evaluating pattern ", pattern)
     print("Result: ", wann.predict(pattern[0]), "Expected: ", pattern[1], "\n")
 
-"""for pattern in test_set:
-    print("Evaluating pattern ", pattern)
-    print("Result: ", wann.predict(pattern[0]), "Expected: ", pattern[1], "\n")
 
-for pattern in test_set:
-    print("Evaluating pattern ", pattern)
-    print("Result: ", wann.predict(pattern[0]), "Expected: ", pattern[1], "\n")
-
-for pattern in test_set:
-    print("Evaluating pattern ", pattern)
-    print("Result: ", wann.predict(pattern[0]), "Expected: ", pattern[1], "\n")
-
-for pattern in test_set:
-    print("Evaluating pattern ", pattern)
-    print("Result: ", wann.predict(pattern[0]), "Expected: ", pattern[1], "\n")
-
-for pattern in test_set:
-    print("Evaluating pattern ", pattern)
-    print("Result: ", wann.predict(pattern[0]), "Expected: ", pattern[1], "\n")
-
-for pattern in test_set:
-    print("Evaluating pattern ", pattern)
-    print("Result: ", wann.predict(pattern[0]), "Expected: ", pattern[1], "\n")
-
-for pattern in test_set:
-    print("Evaluating pattern ", pattern)
-    print("Result: ", wann.predict(pattern[0]), "Expected: ", pattern[1], "\n")
-for pattern in test_set:
-    print("Evaluating pattern ", pattern)
-    print("Result: ", wann.predict(pattern[0]), "Expected: ", pattern[1], "\n")
-for pattern in test_set:
-    print("Evaluating pattern ", pattern)
-    print("Result: ", wann.predict(pattern[0]), "Expected: ", pattern[1], "\n")
-for pattern in test_set:
-    print("Evaluating pattern ", pattern)
-    print("Result: ", wann.predict(pattern[0]), "Expected: ", pattern[1], "\n")
-
-
-"""
-
-wann = Wisard(2, 3546, True)
+wann = Wisard(2, True, 3546)
 wann.train(training_patterns, expected_outputs)
 
+print(wann.get_mental_images("H"))
+
 for discriminator in wann.discriminators:
-    print(discriminator.input_class)
-    print(discriminator.input_length)
-    print(discriminator.tuple_size)
-    print(discriminator.memory)
+    print(wann.discriminators[discriminator_class].input_class)
+    print(wann.discriminators[discriminator_class].input_length)
+    print(wann.discriminators[discriminator_class].tuple_size)
+    print(wann.discriminators[discriminator_class].memory)
 
 
 print("\n\n\n")
@@ -98,14 +65,14 @@ for pattern in test_set:
 
 
 
-wann = Wisard(2, 3546, True)
+wann = Wisard(2, True, 3546)
 wann.train(training_patterns, expected_outputs)
 
 for discriminator in wann.discriminators:
-    print(discriminator.input_class)
-    print(discriminator.input_length)
-    print(discriminator.tuple_size)
-    print(discriminator.memory)
+    print(wann.discriminators[discriminator_class].input_class)
+    print(wann.discriminators[discriminator_class].input_length)
+    print(wann.discriminators[discriminator_class].tuple_size)
+    print(wann.discriminators[discriminator_class].memory)
 
 
 wann.deactivate_bleaching()
@@ -115,3 +82,13 @@ print("\n\n\n")
 for pattern in test_set:
     print("Evaluating pattern ", pattern)
     print("Result: ", wann.predict(pattern[0]), "Expected: ", pattern[1], "\n")
+
+
+mental_images = wann.get_mental_images()
+for mental_image_class in mental_images:
+    mental_image = mental_images[mental_image_class]
+    mental_image = np.asarray(mental_image).reshape((4,3))
+
+    print(mental_image)
+    plt.imshow(mental_image, cmap='hot', interpolation='nearest')
+    plt.show()
