a0b9dea09337ab71d669e13734973b49ed865f3d
Guilherme Caeiro de Mattos <GuilhermeCaeiro@users.noreply.github.com>
2020-04-12 12:25:01 -0300

    Changing training to support online training and added mental image generation.

--- Wisard.py
+++ Wisard.py
@@ -16,7 +16,6 @@ class Discriminator:
     def __init__(self, input_class, input_length, tuple_size, bleaching = False):
         """
         Constructor for the Discriminator class.
-
         :param input_class: string that identifies the class being represented by the discriminator.
         :param input_length: number (integer) of bits in a WiSARD input.
         :param tuple_size: number (integer) that defines the address size to be used by the memories.
@@ -28,13 +27,11 @@ class Discriminator:
         self.ram_size = math.pow(2, self.tuple_size) # Unused
         self.number_of_rams = int(self.input_length / self.tuple_size)
         self.bleaching = bleaching
-        #self.memory = np.zeros(self.number_of_rams, self.ram_size)
         self.memory = {} # This can be a list
     
     def write(self, pattern):
         """
         Writes a pattern to the RAMs.
-
         :param pattern: binary pattern to be learned.
         """
 
@@ -56,10 +53,8 @@ class Discriminator:
     def evaluate(self, pattern, bleaching_threshold = 0):
         """
         Evaluates a pattern and returns its score.
-
         :param pattern: pattern to be evaluated.
         :param bleaching_threshold: threshold to be used to solve draws.
-
         Returns:
             -> discriminator score.
         """
@@ -95,123 +90,105 @@ class Wisard:
     WiSARD "neural network". A weightless, RAM-based classifier.
     """
 
-    def __init__(self, tuple_size = 2, bleaching = False, seed = 0):
+    def __init__(self, tuple_size = 2, bleaching = False, seed = 0, shuffle_observations = True):
         """
         Constructor for the WiSARD class.
-
         :param tuple_size: number (integer) that defines the address size to be used by the memories.
         :param bleaching: boolean indicating if the bleaching functionality is active or not.
         :param seed: integer to be used as seed in the random number generator, to allow reproducibility.
+        :param shuffle_observations: boolean to activate the shuffling of an observation, based on the param "seed".
         """
 
-        #self.input_list = []
-        #self.expected_output_list = []
         self.seed = seed
+        self.shuffle_observations = shuffle_observations
         self.bleaching = bleaching
         self.tuple_size = tuple_size
-        self.discriminators = []
+        self.discriminators = {}
 
-    def process_input(self, mode, input_list, expected_output_list = []):
+
+    def random_mapping(self, observation):
         """
-        Prepares the input by appling to it the random mapping of the bits in the input sequence, based on the seed provided during the class creation. 
-
-        :param mode: string identifying its mode of operation as "training" or "prediction".
-        :param input_list: list of binary input sequences (lists with zeros and ones as integers).
-        :param expected_output_list: list of expected outputs.
-
+        Prepares the observation by appling to it the random mapping of the bits in the input sequence, based on the seed provided during the class creation. 
+        :param observation: list of binary values (lists with zeros and ones as integers).
         Returns: 
-            -> For "training" mode: a dictionary containing the input classes and their respective transformed training observations.
-            -> For "prediction" mode: the transformed observation.
+            -> A shuffled version of "observation"
         """
 
-        # separates classes
-        input_classes = {}
+        # shuffles observation
+        observ_copy = copy.deepcopy(observation)
+        random.seed(self.seed)
+        random.shuffle(observ_copy)
 
-        if mode == "training":
-            for i in range(len(input_list)):
-                if expected_output_list[i] not in input_classes:
-                    input_classes[expected_output_list[i]] = []
-                
-                # shuffles input list
-                input_item = copy.deepcopy(input_list[i])
-                
-                random.seed(self.seed)
-                random.shuffle(input_item)
+        return observ_copy
 
-                input_classes[expected_output_list[i]].append(input_item)
 
-            return input_classes
-        elif mode == "prediction":
-            input_item = copy.deepcopy(input_list[0])
-
-            random.seed(self.seed)
-            random.shuffle(input_item)
-            
-            return input_item
-        else:
-            raise ValueError("Invalid mode identifier.")
-
-    def train(self, input_list, expected_output_list):
+    def train_bulk(self, observations_and_classes):
         """
         Trains the WiSARD classifier based on the provide inputs and its expected outputs. 
+        :param observations_and_classes: list of binary input sequences (lists with zeros and ones as integers) and expected outputs.
+        """
+        for observation_and_classe in observations_and_classes:
+            self.train([observation_and_classe[0]], [observation_and_classe[1]])
 
-        :param input_list: list of binary input sequences (lists with zeros and ones as integers).
-        :param expected_output_list: list of expected outputs (preferrably as strings)
+    def train(self, observations, classes):
+        """
+        Trains the WiSARD classifier based on the provide inputs and its expected outputs. 
+        :param observations: list of binary input sequences (lists with zeros and ones as integers).
+        :param classes: list of expected outputs (preferrably as strings)
         """
 
-        input_classes = self.process_input("training", input_list, expected_output_list)
-        number_of_classes = len(input_classes)
-        class_identifiers = list(input_classes.keys())
-        class_identifiers.sort() # sorts the classes
-        #class_identifiers = class_identifiers[::-1] # reverses the classes' list
-        
-        print("Number of classes being trained: " + str(number_of_classes))
-        print(input_classes.keys())
-        print(class_identifiers)
+        #check lists sizes
+        if(len(observations) != len(classes)):
+            raise Exception("Lengths of \"observations\" and \"classes\" must be equal.")
 
-        # TODO Change to allow online training.
-        for input_class in class_identifiers:
-            print("Number of training samples for class " + str(input_class) + ": " + str(len(input_classes[input_class])))
+        for i in range(len(observations)):
+            observation = observations[i]
+            observation_class = classes[i]
+            discriminator = None
+            
+            if self.shuffle_observations:
+                observation = self.random_mapping(observation)
 
-            input_data_length = len(input_classes[input_class][0])
-            discriminator = Discriminator(input_class, input_data_length, self.tuple_size, self.bleaching)
+            if observation_class not in self.discriminators:
+                observation_length = len(observation)
+                discriminator = Discriminator(observation_class, observation_length, 
+                                    self.tuple_size, self.bleaching)   
+                self.discriminators[observation_class] = discriminator
+            else:
+                discriminator = self.discriminators[observation_class]
 
-            for training_sample in input_classes[input_class]:
-                discriminator.write(training_sample)
+            discriminator.write(observation)
 
-            self.discriminators.append(discriminator)
 
-    def predict(self, rawinput):
+    def predict(self, observation):
         """
-        Evaluates a binary input sequence and returns its class. 
-
-        :param rawinput: binary input sequence (list with zeros and ones as integers).
-
+        Evaluates an observation and returns its predicted class. 
+        :param observation: binary input sequence (list with zeros and ones as integers).
         Returns: the class that returned the biggest discriminator response.
         """
 
-        processed_input = self.process_input("prediction", [rawinput])
+        if self.shuffle_observations:
+            observation = self.random_mapping(observation)
 
         result_achieved = False
 
         predicted_classes = []
         current_threshold = 0
 
-        discriminators_to_evaluate = self.discriminators
+        discriminators_to_evaluate = self.discriminators.keys()
         previously_evaluated_discriminators = []
 
         while not result_achieved:
             predicted_classes = [{"discriminator": None, "score": 0}]
 
-            for discriminator in discriminators_to_evaluate:
-                score, evaluation_pattern, address = discriminator.evaluate(processed_input, current_threshold)
+            for discriminator in sorted(discriminators_to_evaluate):
+                score, evaluation_pattern, address = self.discriminators[discriminator].evaluate(observation, current_threshold)
                 last_score = score
 
                 if score > predicted_classes[0]["score"] or predicted_classes[0]["discriminator"] == None:
-                    predicted_classes = [{"discriminator": discriminator, "score": score}]
+                    predicted_classes = [{"discriminator": self.discriminators[discriminator], "score": score}]
                 elif score == predicted_classes[0]["score"]:
-                    predicted_classes.append({"discriminator": discriminator, "score": score})
-
+                    predicted_classes.append({"discriminator": self.discriminators[discriminator], "score": score})
 
             exit_condition = None
 
@@ -228,7 +205,7 @@ class Wisard:
                     previously_evaluated_discriminators = discriminators_to_evaluate
                     discriminators_to_evaluate = []
                     for predicted_class in predicted_classes:
-                        discriminators_to_evaluate.append(predicted_class["discriminator"])
+                        discriminators_to_evaluate.append(predicted_class["discriminator"].input_class)
 
             elif self.bleaching and len(predicted_classes) == 1:
                 exit_condition = 3
@@ -236,17 +213,6 @@ class Wisard:
             else:
                 print("Error predicting class.")
                 break
-
-            #
-            #if predicted_classes[0]["discriminator"] is None or predicted_classes[0]["score"] is 0:
-            #    print("Last score: ", last_score)
-            #    print("Evaluation pattern: ", evaluation_pattern)
-            #    print("Last address: ", address)
-            #    print("Exit condition: ", exit_condition)
-            #    print("Discriminators: ", discriminators_to_evaluate)
-            #    print("Discriminators: ", previously_evaluated_discriminators)
-            #    print("Prediction failed (score equals 0). Picking the first discriminator's.")
-                
 
         # If the method ends with more than one class as possible, it just returns the first one.
         # TODO: Change the following line to return a random class if there is still a draw between
@@ -259,11 +225,63 @@ class Wisard:
         """
         self.bleaching = False
 
-    def show_mental_map(self):
+    def get_mental_images(self, desired_class = None):
         """
-        Not implemented yet.
+        Generates a mental image for each discriminator. Each mental imagem is 
+        a 1D list that can be converted to an array and reshaped to the desired
+        dimensions. If "desired_class" is provided, only the mental image of the
+        discriminator for the desired class will be generated.
+
+        :param desired_class: class of a given discriminator.
+        
+        Returns: a dictionare of mental images indexed by discriminator's classes.
         """
-        pass
+
+        shuffled_mental_images = {}
+        unshuffled_mental_images = {}
+        input_length = 0
+
+
+        for discriminator_class in self.discriminators:
+            if desired_class != None and discriminator_class != desired_class:
+                continue
+
+            rams = self.discriminators[discriminator_class].memory
+            # not a problem if called multiple time, because all discriminators 
+            # should have the same value for input_length
+            input_length = self.discriminators[discriminator_class].input_length 
+            pixels = []
+
+            for ram in sorted(rams):
+                num_bits = len(list(rams[ram])[0]) #self.tuple_size
+                chunk = [0] * num_bits
+
+                for i in range(num_bits):
+                    for address in rams[ram]:
+                        #if i >= len(address):
+                        #    continue
+
+                        if address[i] == "1" and rams[ram][address] > 0:
+                            chunk[i] = chunk[i] + rams[ram][address]
+                    
+                pixels = pixels + chunk
+
+            shuffled_mental_images[discriminator_class] = pixels
+
+        original_positions = list(range(0,input_length))
+        random.seed(self.seed)
+        random.shuffle(original_positions)
+
+        for mental_image_class in sorted(shuffled_mental_images):
+            shuffled_mental_image = shuffled_mental_images[mental_image_class]
+            unshuffled_mental_image = [0] * len(shuffled_mental_image)
+            
+            for i in range(len(original_positions)):
+                unshuffled_mental_image[original_positions[i]] = shuffled_mental_image[i]
+
+            unshuffled_mental_images[mental_image_class] = unshuffled_mental_image
+
+        return unshuffled_mental_images#, shuffled_mental_images
 
     def save_network_to_disk(self):
         """
@@ -276,4 +294,5 @@ class Wisard:
         Not implemented yet.
         """
         pass
+
         
