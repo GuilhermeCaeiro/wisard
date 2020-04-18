ec1da7e4dea8da61ad484032dab5a1a4ad7e3fb1
Guilherme Caeiro de Mattos <GuilhermeCaeiro@users.noreply.github.com>
2020-04-18 03:09:05 -0300

    Update tests.py

--- tests.py
+++ tests.py
@@ -38,7 +38,7 @@ expected_outputs = [row[1] for row in training_set]
 test_patterns = [row[0] for row in test_set]
 test_expected_outputs = [row[1] for row in test_set]
 
-wann = Wisard(2, False, 3546)
+wann = Wisard(2, False, 3546, type_mem_alloc="dalloc")
 wann.train(training_patterns, expected_outputs)
 print(wann.get_mental_images())
 print_discriminators_info(wann)
@@ -46,11 +46,10 @@ print("\n\n\n")
 for pattern in test_set:
     print("Evaluating pattern ", pattern)
     print("Result: ", wann.predict([pattern[0]]), "Expected: ", pattern[1], "\n")
-wann.print_time_data()
 
 ##########
 
-wann = Wisard(2, True, 3546)
+wann = Wisard(2, True, 3546, type_mem_alloc="palloc")
 wann.train(training_patterns, expected_outputs)
 print(wann.get_mental_images("H"))
 print_discriminators_info(wann)
@@ -58,11 +57,10 @@ print("\n\n\n")
 for pattern in test_set:
     print("Evaluating pattern ", pattern)
     print("Result: ", wann.predict([pattern[0]]), "Expected: ", pattern[1], "\n")
-wann.print_time_data()
 
 ##########
 
-wann = Wisard(2, True, 3546)
+wann = Wisard(2, True, 3546, type_mem_alloc="dalloc")
 wann.train(training_patterns, expected_outputs)
 print(wann.get_mental_images())
 print_discriminators_info(wann)
@@ -71,7 +69,6 @@ wann.deactivate_bleaching()
 for pattern in test_set:
     print("Evaluating pattern ", pattern)
     print("Result: ", wann.predict([pattern[0]]), "Expected: ", pattern[1], "\n")
-wann.print_time_data()
 
 mental_images = wann.get_mental_images()
 for mental_image_class in mental_images:
@@ -82,18 +79,53 @@ for mental_image_class in mental_images:
     plt.imshow(mental_image, cmap='hot', interpolation='nearest')
     plt.show()
 
+##########
+
+print("TRAINING NETWORK.")
+
+wann = Wisard(2, True, 3546, type_mem_alloc="dalloc")
+wann.train(training_patterns, expected_outputs)
+for pattern in test_set:
+    print("Evaluating pattern ", pattern)
+    print("Result: ", wann.predict([pattern[0]]), "Expected: ", pattern[1], "\n")
+
+print("PRINTING MENTAL IMAGE.")
+mental_images = wann.get_mental_images()
+for mental_image_class in mental_images:
+    mental_image = mental_images[mental_image_class]
+    mental_image = np.asarray(mental_image).reshape((4,3))
+    plt.imshow(mental_image, cmap='hot', interpolation='nearest')
+    plt.show()
+
+print("SAVING NETWORK.")
+wann.save("wann.pkl")
+
+print("LOADING NETWORK.")
+wann = Wisard.load("wann.pkl")
+for pattern in test_set:
+    print("Evaluating pattern ", pattern)
+    print("Result: ", wann.predict([pattern[0]]), "Expected: ", pattern[1], "\n")
+
+print("PRINTING MENTAL IMAGE.")
+mental_images = wann.get_mental_images()
+for mental_image_class in mental_images:
+    mental_image = mental_images[mental_image_class]
+    mental_image = np.asarray(mental_image).reshape((4,3))
+    plt.imshow(mental_image, cmap='hot', interpolation='nearest')
+    plt.show()
+
+print("TRAINING ADDITIONAL PATTERNS.")
+wann.train([test_patterns[0]], [test_expected_outputs[0]])
+wann.train([test_patterns[6]], [test_expected_outputs[6]])
+
+print("PRINTING MENTAL IMAGE.")
+mental_images = wann.get_mental_images()
+for mental_image_class in mental_images:
+    mental_image = mental_images[mental_image_class]
+    mental_image = np.asarray(mental_image).reshape((4,3))
+    plt.imshow(mental_image, cmap='hot', interpolation='nearest')
+    plt.show()
+
 ########################################################################
 
-NUM_SAMPLES = 100000
 
-def print_avg_time_type_malloc(type_malloc):
-    wann = Wisard(2, True, 3546, type_mem_alloc=type_malloc)
-    wann.train(training_patterns, expected_outputs)
-    for i in range(NUM_SAMPLES):
-        for pattern in test_set:
-            wann.predict([pattern[0]])
-    wann.print_time_data()
-
-for i in range(0,4):
-    print(i)
-    print_avg_time_type_malloc(i)
